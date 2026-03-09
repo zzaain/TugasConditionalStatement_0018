df90a3a831d5944f878dfdcc6d5ee72470c613b0
zzaain <sheriellaghzain@gmail.com>
2026-03-09 16:57:33 +0700

    membuat prsedur input data berat dan tinggi

--- program_bmi.cpp
+++ program_bmi.cpp
@@ -2,3 +2,12 @@
 using namespace std;
 
 float berat, tinggi; 
+void inputData()
+ {
+
+    cout << "Masukkan Berat Badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan Tinggi Badan (m): ";
+    cin >> tinggi;
+
+}
