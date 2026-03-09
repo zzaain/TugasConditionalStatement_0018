ee53b69d6836b77504303464ee4c3624204661d2
zzaain <sheriellaghzain@gmail.com>
2026-03-09 17:02:21 +0700

    membuat fungsi status BMI, dan menambahkan kondisi kategori BMI

--- program_bmi.cpp
+++ program_bmi.cpp
@@ -17,3 +17,16 @@ float hitungBMI (float a, float b)
     return a / (b * b);
 
 }
+string statusBMI (float bmi)
+{
+    if (bmi < 18.5)
+        return "Berat badan kurang";
+    else if (bmi < 25)
+        return "Berat badan normal";
+    else if (bmi < 30)
+        return "Berat badan kelebihan";
+    else 
+        return "Obesitas";
+        
+
+}
